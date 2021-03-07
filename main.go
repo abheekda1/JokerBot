585f348a372717913de24a85642276ef5212d655
Abheek Dhawan <abheekd@protonmail.com>
2021-03-06 21:42:59 -0600

    Test statistics command

--- main.go
+++ main.go
@@ -147,4 +147,12 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 		s.ChannelMessageSendEmbed(m.ChannelID, embed)
 	}
+
+	if strings.ToLower(m.Content) == "?why so statistical" {
+		embed := &discordgo.MessageEmbed{
+			Color:       0x004400,
+			Description: fmt.Sprintf("Servers: %v", len(s.State.Guilds)),
+		}
+		s.ChannelMessageSendEmbed(m.ChannelID, embed)
+	}
 }
